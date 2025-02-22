eb2e7ace58e69ff79a3a2cfbc9cfd3da45070991
Josue Garcia <GE240098@alumno.udb.edu.sv>
2025-02-22 09:18:42 -0600

    Paths updated

--- api/routes.php
+++ api/routes.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/middleware/auth.php';
 require_once __DIR__ . '/controllers/cursos.php';
 require_once __DIR__ . '/controllers/users.php';
 require_once __DIR__ . '/utils/jwt.php';
